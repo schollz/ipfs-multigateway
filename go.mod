878f5e488ee94122fc9d294f0436cc027b2b9600
Zack Scholl <zack.scholl@gmail.com>
2019-05-08 21:10:25 +0000

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/schollz/super-ipfs-gateway
 
-require github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
+go 1.12
+
+require github.com/schollz/logger v0.0.0-20190508205554-e2bba6fcbabd
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	log "github.com/cihub/seelog"
+	log "github.com/schollz/logger"
 )
 
 func main() {
@@ -21,9 +21,9 @@ func main() {
 
 	// toggle debug mode
 	if debug {
-		setLogLevel("debug")
+		log.SetLevel("debug")
 	} else {
-		setLogLevel("info")
+		log.SetLevel("info")
 	}
 
 	log.Info("running on :" + port)
@@ -148,42 +148,4 @@ func cancelableRequest(result chan *http.Response, cancel chan struct{}, urlToGe
 			return
 		}
 	}
-}
-
-// setLogLevel determines the log level
-func setLogLevel(level string) (err error) {
-
-	// https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
-	// https://github.com/cihub/seelog/wiki/Log-levels
-	appConfig := `
-	<seelog minlevel="` + level + `">
-	<outputs formatid="stdout">
-	<filter levels="debug,trace">
-		<console formatid="debug"/>
-	</filter>
-	<filter levels="info">
-		<console formatid="info"/>
-	</filter>
-	<filter levels="critical,error">
-		<console formatid="error"/>
-	</filter>
-	<filter levels="warn">
-		<console formatid="warn"/>
-	</filter>
-	</outputs>
-	<formats>
-		<format id="stdout"   format="%Date %Time [%LEVEL] %File %FuncShort:%Line %Msg %n" />
-		<format id="debug"   format="%Date %Time %EscM(37)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
-		<format id="info"    format="%Date %Time %EscM(36)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
-		<format id="warn"    format="%Date %Time %EscM(33)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
-		<format id="error"   format="%Date %Time %EscM(31)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
-	</formats>
-	</seelog>
-	`
-	logger, err := log.LoggerFromConfigAsBytes([]byte(appConfig))
-	if err != nil {
-		return
-	}
-	log.ReplaceLogger(logger)
-	return
 }
